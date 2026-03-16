6b301a480b243a1977b62df5003276d939e585fe
TorinoJujakdan <juinwork617@gmail.com>
2026-03-16 20:47:42 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,18 +1,16 @@
-n, m = map(int, input().split())
-
-path = []
-visited = [False] * (n+1)
 def recur(cnt):
     if cnt == m:
         print(*path)
         return
     for i in range(1, n+1):
-        if visited[i]:
-            continue
-        visited[i] = True
-        path.append(i)
-        recur(cnt + 1)
-        path.pop()
-        visited[i] = False
+        if not visited[i]:
+            visited[i] = 1
+            path.append(i)
+            recur(cnt + 1)
+            path.pop()
+            visited[i] = 0
+n, m = map(int, input().split())
+path = []
+visited = [0] * (n+1)
 
 recur(0)
