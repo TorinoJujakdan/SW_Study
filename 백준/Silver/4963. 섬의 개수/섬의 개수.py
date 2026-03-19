18b331f59d1b16423d1c022b4d3fef9b7695dd57
TorinoJujakdan <juinwork617@gmail.com>
2026-03-19 15:20:20 +0900

    [Silver II] Title: 섬의 개수, Time: 84 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,5 +1,4 @@
-import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
 # 8방위 델타배열
 dy = [-1, -1, 0, 1, 1, 1, 0, -1]
 dx = [0, 1, 1, 1, 0, -1 , -1, -1]
@@ -7,30 +6,36 @@ dx = [0, 1, 1, 1, 0, -1 , -1, -1]
 def in_range(y, x):
     return 0 <= y < h and 0 <= x < w
 
-def dfs(y, x):
-    visited[y][x] = True
+def bfs(y, x):
+    q = deque()
+    q.append((y, x))
 
-    for i in range(8):
-        ny, nx = y + dy[i], x + dx[i]
+    while q:
+        cy, cx = q.popleft()
 
-        if in_range(ny, nx) and arr[ny][nx] == 1 and not visited[ny][nx]:
-            dfs(ny, nx)
-        
+        for i in range(8):
+            ny, nx = cy + dy[i], cx + dx[i]
+
+            if in_range(ny, nx) and not visited[ny][nx] and arr[ny][nx] == 1:
+                visited[ny][nx] = 1
+                q.append((ny, nx))
+    
+
 
 while True:
     w, h = map(int, input().split())
-
     if (w, h) == (0, 0):
         break
     else:
         arr = [list(map(int, input().split())) for _ in range(h)]
         visited = [[False] * w for _ in range(h)]
-        
+    
         cnt = 0
         for i in range(h):
             for j in range(w):
-                if arr[i][j] == 1 and not visited[i][j]:
-                    dfs(i, j)
+                if in_range(i, j) and not visited[i][j] and arr[i][j] == 1:
+                    visited[i][j] = 1
+                    bfs(i, j)
                     cnt += 1
 
     print(cnt)
