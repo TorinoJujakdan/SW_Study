017fb52bbb49a575792404ff137b83e58e8011c2
TorinoJujakdan <juinwork617@gmail.com>
2026-03-23 01:35:09 +0900

    [Silver I] Title: 트리 순회, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -4,47 +4,52 @@ class Node:
         self.left = None
         self.right = None
 
+# 전위 순회
 def preorder(node):
     if node:
         print(node.data, end="")
         preorder(node.left)
         preorder(node.right)
 
+# 중위 순회
 def inorder(node):
     if node:
         inorder(node.left)
         print(node.data, end="")
         inorder(node.right)
 
-def postorder(node):
+# 후위 순회
+def postoerder(node):
     if node:
-        postorder(node.left)
-        postorder(node.right)
+        postoerder(node.left)
+        postoerder(node.right)
         print(node.data, end="")
 
 n = int(input())
 
+# 이진 트리 생성
 tree = {}
 
 for i in range(n):
     data, left, right = input().split()
-    
+    # 트리에 해당 노드가 없으면 추가
     if data not in tree:
         tree[data] = Node(data)
     
+
     # 왼쪽 자식이 있다면
-    if left != '.':
+    if left != ".":
         tree[left] = Node(left)
         tree[data].left = tree[left]
     
     # 오른쪽 자식이 있다면
-    if right != '.':
+    if right != ".":
         tree[right] = Node(right)
         tree[data].right = tree[right]
 
-root = tree['A']
+root = tree["A"]
 preorder(root)
 print()
 inorder(root)
 print()
-postorder(root)
+postoerder(root)
